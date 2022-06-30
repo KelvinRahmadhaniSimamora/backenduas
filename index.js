f4554d3a41d1f4e7c4368df14921e49bacf4f0fa
KelvinRahmadhaniSimamora <91173746+KelvinRahmadhaniSimamora@users.noreply.github.com>
2022-06-30 10:27:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+const express = require('express')
+const app = express()
+const port = 5000
+// Connection URL
+const url = 'mongodb+srv://Kelvin:backend_007@cluster0.xllr0.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'backendkelvin';
+
+app.get('/', async (req, res) => {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('tabel');
+    const findResult = await collection.find({}).toArray();
+    res.status(200).json({ uasjson: findResult })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
